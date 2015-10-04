3e9ae282b0b96ecd02a45b89b988d8075d178c78
necisam <king4sam990816@gmail.com>
2015-10-04 13:00:14 +0800

    implement to_tsv,take_tsv

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,12 +7,48 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    read_line = tsv.split("\n")
+    output = []
+    field = []
+    field_name = read_line[0].split("\t")
+    field_name.each { |x| field.push(x)  }
 
+    read_line.drop(1).each{ |aline|
+      temHash = Hash.new
+      tuple = aline.split("\t")
+      tuple.each_index{|index|
+        temHash[field[index]] = tuple[index]
+      }
+      output.push(temHash)
+    }
+    @data=output
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    field = ""
+    @data[0].each{|k,_|
+      if field != ""
+        field.concat("\t"+k)
+      else field.concat(k)
+      end
+    }
+    allstring = ''
+    allstring.concat(field+"\n")
+    @data.each{|tuple|
+      newline = true
+      tuple.each_value{|value|
+        if newline == true
+          allstring << value.to_s
+          newline = false
+        else
+          allstring << "\t" + value.to_s
+        end
+      }
+      allstring << "\n"
+    }
+    allstring
   end
+
 end
