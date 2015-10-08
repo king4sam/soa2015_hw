88a208fb5aee286139dc4acbd5726014569c374a
crazyplum <crazyplum24@gmail.com>
2015-10-08 17:56:24 +0800

    + add new module YamlBuddy in yaml_buddy.rb  - remove methods in flip_flap.rb

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,15 +1,10 @@
 require_relative 'tsv_buddy'
+require_relative 'yaml_buddy'
 require 'yaml'
 
 class FlipFlap
 	# Do NOT create an initialize method
 	include TsvBuddy
+  include YamlBuddy
 
-	def take_yaml(yml)
-  		@data = YAML.load(yml)
-	end
-
-	def to_yaml
-		@data.to_yaml
-	end
 end
---
+++ yaml_buddy.rb
@@ -0,0 +1,13 @@
+module YamlBuddy
+  
+  attr_accessor :data
+
+  def take_yaml(yml)
+    @data = YAML.load(yml)
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
+
+end
